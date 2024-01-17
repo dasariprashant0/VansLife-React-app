8398f20e334a4c939c0278d7288406cec2a11446
Prashant Dasari <dasariprashant0@gmail.com>
2024-01-17 23:49:45 +0530

    Used relative path in the Shared UI and Added Footer on Layout.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { About, Dashboard, Home, Income, Reviews, VanDetail, Vans } from "./pages";
+import {
+  About,
+  Dashboard,
+  Home,
+  Income,
+  Reviews,
+  VanDetail,
+  Vans,
+} from "./pages";
 import "./server";
 import Layout from "./components/Layout";
+import HostLayout from "./components/HostLayout";
 
 function App() {
   return (
@@ -10,12 +19,15 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />}/>
-          <Route path="/host/income" element={<Income />}/>
-          <Route path="host/reviews" element={<Reviews />}/>
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
-    <>
+    <div className="site-wrapper">
       <Header />
-      <Outlet />
-    </>
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
