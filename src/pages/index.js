0fe084dbd40cf6a64f7ce225f4d3c992eed34f83
Prashant Dasari <dasariprashant0@gmail.com>
2024-01-22 01:35:43 +0530

    Correcting import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Home from "./Home";
 import About from "./About";
-import Vans from "./vans/Vans";
+import Vans from "./Vans/Vans";
 import VanDetail from "./Vans/VanDetail";
 import Dashboard from "./Host/Dashboard";
 import Income from "./Host/Income";
