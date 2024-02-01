7ed51ccd40f3166df050e5340a5cdba9df993fe9
Prashant Dasari <dasariprashant0@gmail.com>
2024-02-02 01:26:56 +0530

    Refactor getHostVans() and getHostVan() Function.

--- src/api.js
+++ src/api.js
@@ -1,5 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  getDoc,
+  query,
+  where,
+} from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCflQKOoVxplhmBrSkHqN2llEKmAveg5O4",
@@ -22,36 +30,35 @@ export async function getVans() {
     ...doc.data(),
     id: doc.id,
   }));
-  console.log(dataArr);
   return dataArr;
 }
 
-// export async function getVans(id) {
-//   const url = id ? `/api/vans/${id}` : "/api/vans";
-//   const res = await fetch(url);
-//   if (!res.ok) {
-//     throw {
-//       message: "Failed to fetch vans",
-//       statusText: res.statusText,
-//       status: res.status,
-//     };
-//   }
-//   const data = await res.json();
-//   return data.vans;
-// }
+export async function getVan(id) {
+  const docRef = doc(db, "vans", id);
+  const vanSnapshot = await getDoc(docRef);
+  return {
+    ...vanSnapshot.data(),
+    id: vanSnapshot.id,
+  };
+}
 
-export async function getHostVans(id) {
-  const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw {
-      message: "Failed to fetch vans",
-      statusText: res.statusText,
-      status: res.status,
-    };
-  }
-  const data = await res.json();
-  return data.vans;
+export async function getHostVans() {
+  const q = query(vansCollectionRef, where("hostId", "==", "123"));
+  const querySnapshot = await getDocs(q);
+  const dataArr = querySnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return dataArr;
+}
+
+export async function getHostVan(id) {
+  const docRef = doc(db, "vans", id);
+  const vanSnapshot = await getDoc(docRef);
+  return {
+    ...vanSnapshot.data(),
+    id: vanSnapshot.id,
+  };
 }
 
 export async function loginUser(creds) {
