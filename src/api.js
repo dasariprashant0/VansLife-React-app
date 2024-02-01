55b3c73546769c069dee9553dbc75d901f88414f
Prashant Dasari <dasariprashant0@gmail.com>
2024-02-02 00:58:28 +0530

    Cloud Firestore Setup & Refactoring getVans() Function!

--- src/api.js
+++ src/api.js
@@ -1,16 +1,44 @@
-export async function getVans(id) {
-  const url = id ? `/api/vans/${id}` : "/api/vans";
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw {
-      message: "Failed to fetch vans",
-      statusText: res.statusText,
-      status: res.status,
-    };
-  }
-  const data = await res.json();
-  return data.vans;
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCflQKOoVxplhmBrSkHqN2llEKmAveg5O4",
+  authDomain: "vanlife-2955a.firebaseapp.com",
+  projectId: "vanlife-2955a",
+  storageBucket: "vanlife-2955a.appspot.com",
+  messagingSenderId: "360560747019",
+  appId: "1:360560747019:web:69fcb51957bf61cb1edd0e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const vansCollectionRef = collection(db, "vans");
+
+export async function getVans() {
+  const querySnapshot = await getDocs(vansCollectionRef);
+  const dataArr = querySnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  console.log(dataArr);
+  return dataArr;
 }
+
+// export async function getVans(id) {
+//   const url = id ? `/api/vans/${id}` : "/api/vans";
+//   const res = await fetch(url);
+//   if (!res.ok) {
+//     throw {
+//       message: "Failed to fetch vans",
+//       statusText: res.statusText,
+//       status: res.status,
+//     };
+//   }
+//   const data = await res.json();
+//   return data.vans;
+// }
 
 export async function getHostVans(id) {
   const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
