0fbc25ddd20982dd4812269aae4555d493fa1ba4
Prashant Dasari <dasariprashant0@gmail.com>
2024-02-01 21:19:33 +0530

    Refactoring Await in Vans route.

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import {
   Await,
   Link,
@@ -38,78 +38,82 @@ const Vans = () => {
     );
   }
 
+  function getVansData(vans) {
+    const displayVans = typeFilter
+      ? vans.filter((van) => van.type === typeFilter)
+      : vans;
+
+    const getVansData = displayVans.map((van) => (
+      <div key={van.id} className="van-title">
+        <Link
+          to={van.id}
+          state={{
+            search: `?${searchParams.toString()} `,
+            type: typeFilter,
+          }}
+          aria-label={`View details for ${van.name}, priced $${van.price} / per day`}
+        >
+          <img src={van.imageUrl} alt={`Image of ${van.name}`} />
+          <div className="van-info">
+            <h3>{van.name}</h3>
+            <p>
+              ${van.price} <span>/day</span>
+            </p>
+          </div>
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </Link>
+      </div>
+    ));
+    return (
+      <>
+        <div className="van-list">{getVansData}</div>
+      </>
+    );
+  }
+
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
-      <Await resolve={vansData.vans}>
-        {(vans) => {
-          const displayVans = typeFilter
-            ? vans.filter((van) => van.type === typeFilter)
-            : vans;
+      <div className="van-list-filter-buttons">
+        <button
+          onClick={() => handleFilterChange("type", "simple")}
+          className={`van-type simple ${
+            typeFilter === "simple" ? "selected" : ""
+          }`}
+        >
+          Simple
+        </button>
+        <button
+          onClick={() => handleFilterChange("type", "rugged")}
+          className={`van-type rugged ${
+            typeFilter === "rugged" ? "selected" : ""
+          }`}
+        >
+          Rugged
+        </button>
+        <button
+          onClick={() => handleFilterChange("type", "luxury")}
+          className={`van-type luxury ${
+            typeFilter === "luxury" ? "selected" : ""
+          }`}
+        >
+          Luxury
+        </button>
+        {typeFilter ? (
+          <button
+            onClick={() => handleFilterChange("type", null)}
+            className="van-type clear-filters"
+          >
+            Clear filter
+          </button>
+        ) : null}
+      </div>
 
-          const getVansData = displayVans.map((van) => (
-            <div key={van.id} className="van-title">
-              <Link
-                to={van.id}
-                state={{
-                  search: `?${searchParams.toString()} `,
-                  type: typeFilter,
-                }}
-                aria-label={`View details for ${van.name}, priced $${van.price} / per day`}
-              >
-                <img src={van.imageUrl} alt={`Image of ${van.name}`} />
-                <div className="van-info">
-                  <h3>{van.name}</h3>
-                  <p>
-                    ${van.price} <span>/day</span>
-                  </p>
-                </div>
-                <i className={`van-type ${van.type} selected`}>{van.type}</i>
-              </Link>
-            </div>
-          ));
-          return (
-            <>
-              <div className="van-list-filter-buttons">
-                <button
-                  onClick={() => handleFilterChange("type", "simple")}
-                  className={`van-type simple ${
-                    typeFilter === "simple" ? "selected" : ""
-                  }`}
-                >
-                  Simple
-                </button>
-                <button
-                  onClick={() => handleFilterChange("type", "rugged")}
-                  className={`van-type rugged ${
-                    typeFilter === "rugged" ? "selected" : ""
-                  }`}
-                >
-                  Rugged
-                </button>
-                <button
-                  onClick={() => handleFilterChange("type", "luxury")}
-                  className={`van-type luxury ${
-                    typeFilter === "luxury" ? "selected" : ""
-                  }`}
-                >
-                  Luxury
-                </button>
-                {typeFilter ? (
-                  <button
-                    onClick={() => handleFilterChange("type", null)}
-                    className="van-type clear-filters"
-                  >
-                    Clear filter
-                  </button>
-                ) : null}
-              </div>
-
-              <div className="van-list">{getVansData}</div>
-            </>
-          );
-        }}
-      </Await>
+      <Suspense
+        fallback={<h2 className="data-loader">Loading your vans...</h2>}
+      >
+        <Await resolve={vansData.vans}>{getVansData}</Await>
+      </Suspense>
     </div>
   );
 };
